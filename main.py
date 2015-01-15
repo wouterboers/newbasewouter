b068d0b47772bfab41c1bb04e298ce67ca5e7747
Wouter Boers <wouter.boers@newbase.nl>
2015-01-15 17:22:48 +0100

    first changes

--- main.py
+++ main.py
@@ -1,25 +1,90 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import json
+import cgi
+from google.appengine.ext.webapp.util import run_wsgi_app
+import MySQLdb
+import os
+import logging
+
+
+def getDb():
+    env = os.getenv('SERVER_SOFTWARE')
+    if (env and env.startswith('Google App Engine/')):
+        # Connecting from App Engine
+        db = MySQLdb.connect(
+            unix_socket='/cloudsql/newbase-wouter:database',
+            user='root',
+            db='newbase')
+    else:
+        db = MySQLdb.connect(
+            host='173.194.228.207',
+            port=3306,
+            user='root',
+            passwd='abarth2009',
+            db='newbase')
+    return db
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world version 2!')
+        # for now direct SQL queries, in the future use django as ORM
+        db = getDb();
+        cursor = db.cursor()
+        cursor.execute('SELECT * from projects')
+        output = []
+        for row in cursor.fetchall():
+            output.append(dict([('id', row[0]),
+                                ('name', cgi.escape(row[1]))
+            ]))
 
+        self.response.content_type = 'text/json'
+        self.response.write(output)
+        # other response stuff? https://webapp-improved.appspot.com/guide/response.html
+
+    def post(self):
+        name = self.request.get('name')
+        logging.info('Inserting project named ' + name)
+
+        db = getDb();
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO projects (name) VALUES (%s)', [name])
+        db.commit()
+        db.close()
+
+    def put(self):
+        key = int(self.request.get('id'))
+        name = self.request.get('name')
+        logging.info('Updating project id' + str(key))
+        db = getDb();
+        cursor = db.cursor()
+        cursor.execute('UPDATE projects SET name = (%s) WHERE id = (%s)', [name, key])
+        db.commit()
+        db.close()
+
+class Projects(webapp2.RequestHandler):
+    def delete(self, id):
+        logging.info('Deleting project id' + str(id))
+        db = getDb();
+        cursor = db.cursor()
+        response = cursor.execute('DELETE FROM projects WHERE id = (%s)', [id])
+        db.commit()
+        db.close()
+        if response == 0:
+            self.response.set_status(404)
+            self.response.write(response)
+        else:
+            self.response.write(response)
+
+    def get(self, id):
+        logging.info('Deleting project id' + str(id))
+
+
+
+# webapp2: https://webapp-improved.appspot.com/index.html
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+                                  ('/', MainHandler),
+                                  ('/projects/(\d+)', Projects)
+                              ], debug=True)
